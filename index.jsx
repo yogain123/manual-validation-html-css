a3ac74468e0ed02a488b1ad590e030ad0daac648
yogendra saxena <yogendrasaxena56@gmail.com>
2021-06-29 22:29:39 +0530

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+// import Person from "./Person";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+// import Animal from "./Animal";
+import Demo from "./Demo";
+import { useEffect, useRef, useState } from "react";
+import POC from "./POC";
+
+const queryClient = new QueryClient();
+
+function App() {
+  const [name, setName] = useState("");
+  const [touched, setTouched] = useState({});
+  const [errors, setErrors] = useState({ name: true });
+
+  const validate = (name) => {
+    if (!name) {
+      setErrors({ name: true });
+    } else {
+      setErrors({ name: false });
+    }
+  };
+  return (
+    <div style={{ margin: 100 }}>
+      firstName:{" "}
+      <input
+        type="text"
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+          validate(event.target.value);
+        }}
+        onBlur={(event) => {
+          setTouched({ name: true });
+          setName(event.target.value);
+        }}
+      />
+      {errors?.name && touched?.name && "Name is required"}
+    </div>
+  );
+}
+
+export default App;
